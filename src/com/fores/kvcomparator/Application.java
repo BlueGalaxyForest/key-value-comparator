a9453b96ecd2b2b5e1c5724b819af61a46928d2b
何懋林 <2211634606@qq.com>
2023-12-01 18:43:20 +0800

    更新...

--- src/com/fores/kvcomparator/Application.java
+++ src/com/fores/kvcomparator/Application.java
@@ -8,8 +8,8 @@ public class Application {
 
         String path1 = "C:\\Users\\Administrator\\Desktop\\GitHub项目\\key-value-comparator\\files\\PC.txt";
         String path2 = "C:\\Users\\Administrator\\Desktop\\GitHub项目\\key-value-comparator\\files\\小程序.txt";
-        fileKeyValueComparator.compare(path1,path2,"PC的直接提交","小程序的直接提交");
-//        responseKeyValueComparator.compare(path1,path2,"58459案件","176699案件");
+        fileKeyValueComparator.compare(path1, path2, "PC的直接提交", "小程序的直接提交");
+
 
     }
 }
