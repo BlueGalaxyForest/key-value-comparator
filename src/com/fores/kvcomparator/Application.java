ce7f5b1f53cdf19ce042a1e99156a945dd0fe889
何懋林 <2211634606@qq.com>
2024-06-02 23:54:06 +0800

    提交from_home

--- src/com/fores/kvcomparator/Application.java
+++ src/com/fores/kvcomparator/Application.java
@@ -6,8 +6,8 @@ public class Application {
     public static void main(String[] args) {
         FileKeyValueComparator fileKeyValueComparator = new FileKeyValueComparator();
 
-        String path1 = "C:\\Users\\Administrator\\Desktop\\GitHub项目\\key-value-comparator\\files\\PC.txt";
-        String path2 = "C:\\Users\\Administrator\\Desktop\\GitHub项目\\key-value-comparator\\files\\小程序.txt";
+        String path1 = "G:\\软件项目区\\key-value-comparator\\files\\PC.txt";
+        String path2 = "G:\\软件项目区\\key-value-comparator\\files\\小程序.txt";
         fileKeyValueComparator.compare(path1, path2, "PC的直接提交", "小程序的直接提交");
 
 
